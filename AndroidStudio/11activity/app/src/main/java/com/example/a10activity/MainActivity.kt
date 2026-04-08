c2d1387143b6c1a66d35ead6e8bddbf8dfa3b205
vinnavannewton <vinnavannewton294@gmail.com>
2026-04-08 15:56:14 +0530

    updt

--- AndroidStudio/11activity/app/src/main/java/com/example/a10activity/MainActivity.kt
+++ AndroidStudio/11activity/app/src/main/java/com/example/a10activity/MainActivity.kt
@@ -4,13 +4,17 @@ package com.example.a10activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//GO from second act to first
 class MainActivity : AppCompatActivity() {
     private lateinit var goToButton: Button
+    private lateinit var textViewResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +24,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val getResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if(it.resultCode == Constants.RESULT_CODE) {
+                    val message = it.data!!.getStringExtra((Constants.INTENT_MESSAGE2_KEY))
+                    textViewResult.text = message
+                }
+            }
+        textViewResult = findViewById(R.id.textView)
         goToButton = findViewById(R.id.button_go_to_act)
         goToButton.setOnClickListener {
             Intent(this@MainActivity, SecondActivity::class.java).also {
@@ -27,8 +39,9 @@ class MainActivity : AppCompatActivity() {
                 it.putExtra(Constants.INTENT_MESSAGE2_KEY, "How was your day?")
                 it.putExtra(Constants.INTENT_DATA_NUMBER, 3.14)
                 startActivity(it)
+                getResult.launch(intent)
 
-            } //Go from main activity to second activity
+            }
         }
     }
-}
+} //20:37:09
---
+++ AndroidStudio/11activity/app/src/main/java/com/example/a10activity/utils.kt
@@ -0,0 +1,10 @@
+package com.example.a10activity
+
+class Constants {
+    companion object {
+        const val INTENT_MESSAGE_KEY = "Message"
+        const val INTENT_MESSAGE2_KEY = "Message2"
+        const val INTENT_DATA_NUMBER = "Number"
+        const val RESULT_CODE = 123456
+    }
+}
